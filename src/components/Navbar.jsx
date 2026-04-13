946fed65e3b477469c9b7680976ee33dc4965415
5aaryan6k <aaryankoirala20@gmail.com>
2026-04-13 14:55:53 +0545

    Added Home too

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,6 +26,7 @@ const Navbar = () => {
         {/* Nav Links - Centered using absolute positioning */}
         <div className="hidden md:flex absolute left-1/2 transform -translate-x-1/2">
           <div className="flex items-center gap-6 lg:gap-8 text-xs uppercase tracking-widest font-medium">
+            <a href="#HeroSection" className='nav-link'>Home</a>
             <a href="#about" className="nav-link">About</a>
             <a href="#projects" className="nav-link">Projects</a>
             <a href="#skills" className="nav-link">Skills</a>
